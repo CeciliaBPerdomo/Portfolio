5beb8c7fb59d1a2b8415affe82ac795659dcc39d
Cecilia <cecilia.perdomo@gmail.com>
2023-06-28 11:38:52 -0300

    Acerca de mi y contacto

--- src/front/js/component/contacto.jsx
+++ src/front/js/component/contacto.jsx
@@ -4,30 +4,36 @@ import React from "react";
 export const Contacto = () => {
 	return (
 		<div className="container">
-            <div className="mb-3">
-                <label for="exampleFormControlInput1" className="form-label">Nombre:</label>
-                <input type="email" className="form-control" id="exampleFormControlInput1" 
-                placeholder="Nombre y Apellido" />
-            </div>
+            <form 
+            action="https://formspree.io/f/xjvzeqpb"
+            method="POST">
+                <div className="mb-3">
+                    <label for="exampleFormControlInput1" className="form-label">Nombre:</label>
+                    <input type="text" className="form-control" id="exampleFormControlInput1" 
+                    placeholder="Nombre y Apellido" name="Nombre y apellido: "/>
+                </div>
 
-            <div className="mb-3">
-                <label for="exampleFormControlInput1" className="form-label">Email:</label>
-                <input type="email" className="form-control" id="exampleFormControlInput1" 
-                placeholder="name@example.com" />
-            </div>
-            
-            <div className="mb-3">
-                <label for="exampleFormControlTextarea1" className="form-label">Mensaje: </label>
-                <textarea className="form-control" id="exampleFormControlTextarea1"
-                placeholder="Mensaje" rows="3"></textarea>
-            </div>
+                <div className="mb-3">
+                    <label for="exampleFormControlInput1" className="form-label">Email:</label>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" 
+                    placeholder="name@example.com" name="E-mail: " />
+                </div>
+                
+                <div className="mb-3">
+                    <label for="exampleFormControlTextarea1" className="form-label">Mensaje: </label>
+                    <textarea className="form-control" id="exampleFormControlTextarea1"
+                    placeholder="Mensaje" rows="3" name="Mensaje: "></textarea>
+                </div>
 
-            <div className="mb-3 d-flex justify-content-end">
-                <button type="button" className="btn btn-outline-danger"
-                style={{width: "320px"}}>
-                    Enviar
-                </button>
-            </div>
+                <input name="subject" value="Contacto desde https://ceciliaperdomo.vercel.com" hidden="true"/>
+
+                <div className="mb-3 d-flex justify-content-end">
+                    <button type="submit" className="btn btn-outline-danger"
+                    style={{width: "320px"}}>
+                        Enviar
+                    </button>
+                </div>
+            </form>
         </div>
     )
 }
--- src/front/js/pages/aboutMe.js
+++ src/front/js/pages/aboutMe.js
@@ -16,44 +16,76 @@ export const AboutMe = () => {
                     </div>
                     
                     <div className="col">
-                            < br/>  < br/>
-                        <p style={{fontFamily: "Nanum Gothic Coding, monospace", 
-						fontSize: "21px"}}>
-                        Soy Cecilia Perdomo, <b>Full Stack Software Developer</b>, 
-                        <b>Analista Programadora</b> y próxima <b>Diseñadora UX/ UI</b>. 
                         < br/>  < br/>
+                        <p style={{fontFamily: "Quicksand, sans-serif", 
+						fontSize: "24px"}}>
+                        Soy Cecilia Perdomo, <b>Full Stack Software Developer</b>, 
+                        <b> Analista Programadora</b> y <b>Diseñadora UX/ UI</b>. 
+                        < br/>  < br/>
+                        Soy entusiasta, investigadora y curiosa por naturaleza. Constante y detallista ... y a veces creativa.
+                        < br/> 
+                        Me encanta saber, que en la programación, <b>el límite es la imaginación</b>. 
+                        
+{/*                         
+                        <br/><br/>
+                        
                         Te invito a descargar mi curriculum si quieres conocer mi historial detallado.
+                       
                         < br/>
-                        <img src={down} style={{width: "45px"}}/>
+
+                        <a href={pdf} 
+                        style={{textDecoration: "none", color: "black"}}>
+                            <img src={down} style={{width: "45px"}}/>
+                        </a> */}
                         < br/>< br/>
-                        Mas abajo podrás ver mis certificaciones más recientes.
+
+                        Mas abajo podrás ver mis algunas de mis certificaciones.
                         </p>
                         <img src={flecha} style={{width: "55px"}}/>
                     </div>
                 </div>
 
+                < br/>< br/>
                 <div className="row">
                     <div className="col">
-                        <p style={{fontFamily: "Nanum Gothic Coding, monospace", 
-						fontSize: "21px", textAlign: "left"}}>
-                        <br /><br />
-                        Como te definirías? <br />
-                        Entusiasta, investigadora y curiosa por naturaleza. Constante y detallista ... y a veces creativa.
-
-                        <br /><br />
-                        Pero ... y por que te dedicas a esto?<br />
-                        Me encanta saber, que en la programación, el único límite para crear es la imaginación. 
-
-                        <br /><br />
-                        No te da miedo? <br />
-                        Si, claro, pero de a poco somos cada vez mas las mujeres que nos animamos a hacer girar la rueda en este mundo de tecnología.
-
-                        <br /><br />
-                        Que estudiaste para llegar hasta aca? <br />
-                        
-                    </p>
+                        <b>Diseñadora UX/UI</b>
+                        <br /> <br /> <br />
+                        <div class="spinner-border text-danger" role="status">
+                            <span class="visually-hidden">Loading...</span>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <b>Full Stack Software Developer</b>
+                        <img src="https://media.licdn.com/dms/image/C4D2DAQF5AfIgPjHd_A/profile-treasury-image-shrink_800_800/0/1669830409899?e=1688569200&v=beta&t=mM31pOowWUo9bL8XLePeFGvbdVjxCeoinsYU3tGbfzY" 
+                        style={{width: "350px"}}/>
+                    </div>
+                    <div className="col">
+                        <b>Programación Backend</b>
+                        <img src="https://media.licdn.com/dms/image/D4D2DAQHmPq8s9dnzNg/profile-treasury-image-shrink_800_800/0/1665868040450?e=1688569200&v=beta&t=HgioF6IoEYT3PkE_WV1ZHOrCZIhvbiq3mIhcprUWjFs"
+                        style={{width: "250px"}} />
                     </div>
                 </div>
+
+                < br/>< br/>
+
+                <div className="row">
+                    <div className="col">
+                        <b>Introducción a la programación</b>
+                        <img src="https://media.licdn.com/dms/image/C4D2DAQFlSkH1NhNKQA/profile-treasury-image-shrink_800_800/0/1670024166179?e=1688569200&v=beta&t=rLN94lcOJrImQx5uB0GTSltP3NMVswwpOzJknc-GwQk" 
+                        style={{width: "438px"}} />
+                    </div>
+                    <div className="col">
+                        <b>Programador JavaScript - FrontEnd</b>
+                        <img src="https://media.licdn.com/dms/image/D4D2DAQGl5vVLq0Lviw/profile-treasury-image-shrink_800_800/0/1687961649910?e=1688569200&v=beta&t=LmbMUZvUFQRY7lhmMnXkqblPwuqSsm9Ih6uCWo3LlL0" 
+                        style={{width: "350px"}}/>
+                    </div>
+                    <div className="col">
+                        <b>Software Tester</b>
+                        <img src="https://media.licdn.com/dms/image/D4D2DAQGKGlDe_hMivg/profile-treasury-image-shrink_800_800/0/1687962434886?e=1688569200&v=beta&t=gKU4pb_CxSRwBCYQhgddWLSoTCKvp6pSuAW8leikCvU"
+                        style={{width: "350px"}} />
+                    </div>
+                </div>
+
             </div>
             < br/>  < br/>
         </div>
